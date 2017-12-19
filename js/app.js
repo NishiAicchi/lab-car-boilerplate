b59efeec6e3b5d3034ae21a943a562d670779c02
NishiAicchi <ai.espichan@gmail.com>
2017-12-19 18:31:47 -0500

    añadiendo funcionalidad a navbar

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 // La funcionalidad de tu proyecto
+$(document).ready(begin);
+$("#myModal").modal();
+
+
+//google maps
 function initMap() {
     var uluru = {lat: -25.363, lng: 131.044};
     var map = new google.maps.Map(document.getElementById('map'), {
