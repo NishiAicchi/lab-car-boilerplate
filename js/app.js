e46ca990e26eda8e2bf0b0065823fda0ce13f1d9
NishiAicchi <ai.espichan@gmail.com>
2017-12-20 18:05:18 -0500

    añadiendo estructura del primer modal

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 // La funcionalidad de tu proyecto
+$(document).ready(begin);
+
+$('#myModal').modal();
+
+
+
+
 function initMap() {
     var uluru = {lat: -25.363, lng: 131.044};
     var map = new google.maps.Map(document.getElementById('map'), {
